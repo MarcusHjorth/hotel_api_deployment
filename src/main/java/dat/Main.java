24320554bab3faba6e742194d0e0e30ec3de8ca3
Marcus Rasmussen <marcusphr1808@gmail.com>
2024-12-05 18:17:12 +0100

    Main comment

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -15,6 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         Populate.main(args);
-        ApplicationConfig.startServer(7070);
+        ApplicationConfig.startServer(7070); // sander er noob
     }
 }
